e15d722b29cb34240a8e0d526de03b4fbc9522af
prashana-sht <shrestha.prashana@gmail.com>
2023-10-31 23:26:33 +0545

    added signin in landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,11 @@
+import React from "react";
+import Signin from "../(auth)/signin/page";
 export default function LandingPage() {
-  return (
-    <section className="w-full h-full flex items-center justify-center">
-      <div>initial landing page </div>
+  return (<> 
+  <section className="w-full h-full flex justify-center items-center">
+      <p className="text-lg font-semibold font mt-7" style={{ fontSize: '35px' }}>Welcome to Jiffy Chat </p>
     </section>
+    <Signin />
+    </>
   );
 }
