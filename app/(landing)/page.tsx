54adc3cbda8e0f1c42fade05e793455a80ee7bf5
Sagar Chand <chandsagar314@gmail.com>
2023-11-02 19:32:48 +0545

    some changes

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -3,7 +3,7 @@ import Signin from "../(auth)/signin/page";
 export default function LandingPage() {
   return (<> 
   <section className="w-full h-full flex justify-center items-center">
-      <p className="text-lg font-semibold font mt-7" style={{ fontSize: '35px' }}>Welcome to Jiffy Chat </p>
+      <p className="text-lg font-semibold font mt-6" style={{ fontSize: '35px' }}>Welcome to Jiffy Chat </p>
     </section>
     <Signin />
     </>
