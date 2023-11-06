1c90c385310b0e0f77c6f1db71c7e6a811b526d2
Sagar Chand <chandsagar314@gmail.com>
2023-11-06 18:09:17 +0545

    working on signin

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,9 +1,9 @@
 import Logo from "@/components/custom/Logo";
-import Signin from "@/app/(auth)/signin/page";
+import Signin from "@/components/Signin";
 
 export default function HomePage() {
   return (
-    <section className="w-full min-h-screen flex justify-center items-center py-10 sm:px-8 lg:px-10">
+    <section className="w-full min-h-screen flex flex-col justify-center items-center py-10 sm:px-8 lg:px-10">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <Logo />
         <h2 className="mt-6 text-center text-3xl font-bold tracking-tighter">
