4a2cdf9c120389154693387e76ffefc25a894ad8
Sagar Chand <chandsagar314@gmail.com>
2024-01-09 15:53:21 +0545

    changes in messagebar

--- components/chat/MessageBar.tsx
+++ components/chat/MessageBar.tsx
@@ -1,7 +1,7 @@
 import { BsEmojiSmile } from "react-icons/bs";
 import { ImAttachment } from "react-icons/im";
 import { MdSend } from "react-icons/md";
-// import { FaMicrophone } from "react-icons/fa";
+import { FaMicrophone } from "react-icons/fa";
 
 import { Button } from "@/components/ui/button";
 import { RootState, useAppSelector } from "@/redux/store";
@@ -12,6 +12,15 @@ const MessageBar = () => {
     (state: RootState) => state.contactList
   );
   const [message, setMessage] = useState("");
+  const [isTyping, setIsTyping] = useState(false);
+
+  const handleFocus = () => {
+    setIsTyping(true);
+  };
+
+  const handleBlur = () => {
+    setIsTyping(false);
+  };
 
   return (
     <>
@@ -34,19 +43,27 @@ const MessageBar = () => {
               className="bg-gray-600 text-sm focus:outline-none text-white placeholder:text-white h-10 rounded px-5 py-4 w-full"
               value={message}
               onChange={(e) => setMessage(e.target.value)}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
             />
           </li>
           <li className="flex-center w-10">
-            <Button className="px-5 flex-center">
-              <MdSend
-                className="text-gray-200 cursor-pointer text-xl"
-                title="send message"
-              />
-            </Button>
-            {/* <FaMicrophone
-          className="text-gray-200 cursor-pointer text-xl"
-          title="record"
-        /> */}
+            {isTyping ? (
+              <Button className="px-5 flex-center">
+                <MdSend
+                  className="text-gray-200 cursor-pointer text-xl"
+                  title="send message"
+                />
+              </Button>
+            ) : (
+              <div className="px-5">
+                <FaMicrophone
+                  className="text-gray-200 cursor-pointer text-xl"
+                  title="record"
+                  size={25}
+                />
+              </div>
+            )}
           </li>
         </ul>
       )}
