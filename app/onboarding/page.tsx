a25021be8e0cd780b6a581daf30b4c1b979a411c
prashana-sht <shrestha.prashana@gmail.com>
2024-01-13 21:20:56 +0545

    working in onboarding page

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { useState } from 'react';
 import Image from "next/image";
 import Input from "@/components/common/Input";
-import Avatar from "@/components/common/Avatar";
+import Avatar from "@/components/common/OnboardingAvatar";
 import Gender from "@/components/common/Gender";
 
 const Onboarding = () => {
